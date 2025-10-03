6fd95bde0471b71a5dc05430a6d1a22ce5eabf22
BuiHuuTrung123 <trungbuimanifest@gmail.com>
2025-10-03 15:33:26 +0700

    final

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const WHITELIST_DOMAINS = [
     //'http://localhost:5173',
-'https://react-js-material-ui-5s26.vercel.app/'
+'https://react-js-material-ui-5s26.vercel.app'
 ]
 export const BOARD_TYPES = {
     PRIVATE:'private',
