29df60de574e0c38dfefbe2b8a985f3d18e47889
BuiHuuTrung123 <trungbuimanifest@gmail.com>
2025-10-16 15:50:59 +0700

    api-updatecard

--- src/validations/cardValidation.js
+++ src/validations/cardValidation.js
@@ -23,7 +23,27 @@ const createNew = async (req, res, next) => {
 
 }
 
+const update = async (req, res, next) => {
+    // Lưu ý không dùng hàm required() trong trường hợp Update
+    const correctiondition = joi.object({
+        title: joi.string().min(3).max(50).trim().strict(),
+        description: joi.string().optional()
+    })
+    
+    try {
+        // Chỉ định abortEarly: false để trường hợp có nhiều lỗi validation thì trả về tất cả
+        // Đối với trường hợp update, cho phép Unknown để không cần đẩy một số field lên
+        await correctiondition.validateAsync(req.body, {
+            abortEarly: false,
+            allowUnknown: true
+        })
 
+        next()
+    } catch (error) {
+        next(new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, new Error(error).message))
+    }
+}
 export const cardValidation = {
-    createNew
+    createNew, 
+    update
 }
